a675e37d71f4cd704327a4e181c708078c5859e9
Ishan <ishanwijes@gmail.com>
2023-05-14 19:11:14 +1000

    added bootstrap styles

--- nextjs-typescript-course/pages/_app.tsx
+++ nextjs-typescript-course/pages/_app.tsx
@@ -1,6 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Container } from 'react-bootstrap'
+import styles from '@/styles/App.module.css'
+
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -8,7 +13,16 @@ const inter = Inter({ subsets: ['latin'] })
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
-      <Component {...pageProps} />
+      <Head>
+        <title>NextJS News App</title>
+        <meta name="description" content="NextJS News App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Container className={styles.pageContainer}>
+        <Component {...pageProps} />
+      </Container>
     </div>
   )
 }
